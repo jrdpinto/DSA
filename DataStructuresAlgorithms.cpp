86685536428312a11ed1533078f8f08ad51234d5
Pinto <japinto@ea.com>
2021-02-16 21:55:36 +1100

    Initial commit

---
+++ DataStructuresAlgorithms.cpp
@@ -0,0 +1,12 @@
+﻿// DataStructuresAlgorithms.cpp : Defines the entry point for the application.
+//
+
+#include "DataStructuresAlgorithms.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
