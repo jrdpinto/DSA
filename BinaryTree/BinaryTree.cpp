f37be013e2be5ac3ad6cacdba929951e269e11e0
jazza <>
2023-07-13 22:15:31 +1000

    Modified make files to allow for directories and sub-directories to be automatically added.

--- BinaryTree/BinaryTree.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "BinaryTree.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
---
+++ BinaryTree/C++/BinaryTree.cpp
@@ -0,0 +1,16 @@
+﻿#include "BinaryTree.h"
+
+using namespace std;
+
+int unsortedNumbers[] = { 0, 1, 3, 453, 233, 43, 123, 423 ,23, 102, 43, 42, 65, 4455, 20 };
+
+int main()
+{
+	BinaryTreeNode<int> rootNode(0);
+	for (int i = 0; i < sizeof(unsortedNumbers) / sizeof(int); ++i)
+	{
+		rootNode.Add(unsortedNumbers[i]);
+	}
+
+	return 0;
+}
