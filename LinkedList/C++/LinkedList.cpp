ffce47ae73f865a9ff29824e61e0d9a67344e215
jazza <>
2023-08-31 13:47:26 +1000

    Added initial Linked List class.

--- LinkedList/C++/LinkedList.cpp
+++ LinkedList/C++/LinkedList.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
 	cout << "Hello CMake." << endl;
+
+	{
+		LinkedList<int> testList(1);
+		testList.append(2);
+		testList.append(3);
+		testList.append(4);
+		testList.append(5);
+	}
+
 	return 0;
 }
--- LinkedList/C++/LinkedList.h
+++ LinkedList/C++/LinkedList.h
@@ -1,8 +1,70 @@
-﻿// DataStructuresAlgorithms.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#pragma once
 #include <iostream>
 
-// TODO: Reference additional headers your program requires here.
+
+template <typename T>
+class LinkedList
+{
+private:
+	struct Node
+	{
+		T value;
+		Node* next = nullptr;
+	
+		Node(T value)
+		{
+			this->value = value;
+		}
+	};
+
+	Node* head_;
+	Node* tail_;
+	int length_ = 0;
+
+public:
+	LinkedList(T value)
+	{
+		Node* newNode = new Node(value);
+		head_ = newNode;
+		tail_ = newNode;
+		length_ = 1;
+	}
+
+	~LinkedList()
+	{
+		int count = 0;
+		Node* currentNode = head_;
+		while (currentNode != nullptr)
+		{
+			Node* nextNode = nullptr;
+			if (currentNode->next)
+			{
+				nextNode = currentNode->next;
+			}
+
+			delete(currentNode);
+			currentNode = nextNode;
+			++count;
+
+			_ASSERT(count <= length_);
+		}
+	}
+
+	void append(T value)
+	{
+		Node* newNode = new Node(value);
+		Node* previousTail = tail_;
+		previousTail->next = newNode;
+		tail_ = newNode;
+		++length_;
+	}
+
+	void prepend(T value)
+	{
+		Node<T>* newNode = new Node<T>(value);
+		Node<T>* previousHead = head_;
+		head_ = newNode;
+		head_->next = previousHead;
+		++length_;
+	}
+};
