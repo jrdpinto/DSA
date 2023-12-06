3b1c48b0a5a93c46c2b11fe76e87ad495bb2d791
jazza <>
2023-12-06 11:50:59 +1100

    Stack - Added 'ReverseString()' interview question.

--- Stacks/C++/Stack.cpp
+++ Stacks/C++/Stack.cpp
@@ -1,4 +1,43 @@
 #include "Stack.h"
+#include <string>
+
+// Interview Question - Reverse string
+// Implement a function called reverseString() that reverses the input string using a stack data structure.
+// 
+// Input:
+// The function takes a constant reference to a string str.
+// 
+// Output:
+// The function should return a new string that is the reverse of the input string str.
+// 
+// Constraints:
+// The input string may contain any printable ASCII characters.
+// 
+// Examples:
+// Consider the following input string:
+//     str: "hello"
+// 
+// After calling reverseString(str), the result should be:
+// 
+//     "olleh"
+std::string ReverseString(std::string& inputString)
+{
+    std::string reversedString;
+
+    Stack<char> charStack;
+    for (int i = 0; i < inputString.size(); ++i)
+    {
+        charStack.Push(inputString[i]);
+    }
+
+    while (charStack.GetHeight() > 0)
+    {
+        reversedString += charStack.Pop();
+    }
+
+    return reversedString;
+}
+
 
 int main()
 {
@@ -22,6 +61,13 @@ int main()
             std::cout << "Popped value: " << poppedValue << std::endl;
             testStack.Print();
         }
+    }
+
+    {
+        std::cout << "Reverse string test" << std::endl;
+        std::string inputString("Hello!");
+        std::cout << "Input string " << inputString.c_str() << std::endl;
+        std::cout << "Output: " << ReverseString(inputString).c_str() << std::endl;
     }
 
     return 0;
