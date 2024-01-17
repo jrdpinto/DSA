f9b954e835b5ae335bb55ba4ee6bd40f90404f89
jazza <>
2024-01-17 18:11:05 +1100

    Hash Table - Added interview 'Two Sum' interview question.

--- HashTable/C++/HashTable.cpp
+++ HashTable/C++/HashTable.cpp
@@ -1,6 +1,7 @@
 #include "HashTable.h"
 #include <unordered_map>
 #include <algorithm>
+#include <string>
 
 // Interview question - Find first non-repeating character in a string
 // Implement a function called firstNonRepeatingChar() that finds and returns the first non-repeating
@@ -53,13 +54,15 @@ char FirstNonRepeatingChar(const std::string& inputString)
     return nonRecurringCharacter;
 }
 
-// Implement a function called groupAnagrams() that groups a list of strings based on their anagram equivalence.
+// Implement a function called groupAnagrams() that groups a list of strings based on their
+// anagram equivalence.
 // Input:
 // The function takes a constant reference to a vector of strings strings.
 // 
 // Output:
-// The function should return a vector of vectors of strings, where each inner vector contains a group of
-// anagram-equivalent strings. The order of the groups and the strings within the groups does not matter.
+// The function should return a vector of vectors of strings, where each inner vector contains
+// a group of anagram-equivalent strings. The order of the groups and the strings within the 
+// groups does not matter.
 // 
 // Constraints:
 //     The input strings may contain any printable ASCII characters.
@@ -69,7 +72,8 @@ char FirstNonRepeatingChar(const std::string& inputString)
 // Consider the following input vector:
 //     strings: {"eat", "tea", "tan", "ate", "nat", "bat"}
 // 
-// After calling groupAnagrams(strings), the result could be (order of groups and strings within groups does not matter):
+// After calling groupAnagrams(strings), the result could be (order of groups and strings within
+// groups does not matter):
 //     {
 //         {"eat", "tea", "ate"},
 //         {"tan", "nat"},
@@ -102,6 +106,61 @@ std::vector<std::vector<std::string>> GroupAnagrams(const std::vector<std::strin
     }
 
     return result;
+}
+
+// Implement a function called twoSum() that finds two numbers in a given vector of integers that 
+// add up to a specific target number. If there are two pairs of numbers that sum to the target
+// it is assumed that you're looking for the first pair of numbers that sum to the target, based
+// on their position in the array (an example is given below).
+// 
+// Input:
+// The function takes the following inputs:
+//     A constant reference to a vector of integers nums.
+//     An integer target.
+// 
+// Output:
+// The function should return a vector of two integers representing the indices of the two numbers
+// in the input vector nums that add up to the target value. If no such pair exists, the function
+// should return an empty vector.
+// 
+// Constraints:
+//     Each input would have exactly one solution.
+//     The same element in the input vector should not be used twice.
+// 
+// Example:
+// Consider the following input vector and target:
+//     nums: {1, 3, 4, 2, 5};
+//     target: 6;
+// 
+// There are two combinations that add up to 6 which are:
+//     {2, 3} and {0, 4}
+// 
+// After calling twoSum(nums, target), the result should be the first pair of numbers that sum to the target:
+//     {2, 3}
+std::vector<int> TwoSum(std::vector<int>& nums, int target)
+{
+    std::unordered_map<int, int> numToIndex;
+	std::vector<int> result;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        int num = nums[i];
+        int complement = target - num;
+
+        auto it = numToIndex.find(target-num);
+        if (it != numToIndex.end())
+        {
+            result.push_back(numToIndex[complement]);
+            result.push_back(i);
+            break;
+        }
+        else
+        {
+            numToIndex[num] = i;
+        }
+    }
+	
+	return result;
 }
 
 int main()
@@ -142,6 +201,19 @@ int main()
             }
             std::cout << "}" << std::endl;
         }
+    }
+
+    {
+        std::cout << "Interview question - TwoSum." << std::endl;
+        std::vector<int> result = TwoSum(std::vector<int>{1, 3, 4, 2, 5}, 6);
+        std::cout << "{1, 3, 4, 2, 5} Result:" << std::endl;
+        std::string output;
+        for (int num : result)
+        {
+            output.append(std::to_string(num));
+            output.append(" ");
+        }
+        std::cout << output.c_str() << std::endl;
     }
 
     return 0;
