cf6e142d3748615cf44c4fb515b23fbeb01ccb8d
jp <jpgitpublic@gmail.com>
2024-02-28 13:22:57 +1100

    RedBlackTree - Removed AVL code.

--- Trees/RedBlackTree/C++/RedBlackTree.cpp
+++ Trees/RedBlackTree/C++/RedBlackTree.cpp
@@ -4,29 +4,7 @@
 
 int main()
 {
-    std::cout << "AVL Tree" << std::endl;
-
-    {
-        auto avlTreeTest = [] (std::vector<int>& values)
-            {
-                RedBlackTree<int> tree;
-                std::cout << "Inserting: ";
-                for (int value : values)
-                {
-                    std::cout << value << " ";
-                    tree.Insert(value);
-                }
-                std::cout << std::endl;
-
-                std::cout << "Tree is " << (tree.IsHeightBalanced() ? "height balanced" : 
-                    "not height balanced") << std::endl;
-            };
-
-        avlTreeTest(std::vector<int>{3, 9, 20, 15, 7});
-        avlTreeTest(std::vector<int>{3, 9, 7, 15, 20});
-        avlTreeTest(std::vector<int>{20, 19, 18, 16, 8});
-        avlTreeTest(std::vector<int>{20, 20, 19, 18, 16, 8});
-    }
+    std::cout << "RedBlack Tree" << std::endl;
 
     return 0;
 }
