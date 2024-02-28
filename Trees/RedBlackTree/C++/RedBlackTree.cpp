93a4605dd44f19a3f513b5fc7c34c46eef49b628
jp <jpgitpublic@gmail.com>
2024-02-28 13:10:39 +1100

    AVLTree - Added insertion, rotate left, rotate right.

--- Trees/RedBlackTree/C++/RedBlackTree.cpp
+++ Trees/RedBlackTree/C++/RedBlackTree.cpp
@@ -1,9 +1,32 @@
 
 #include "RedBlackTree.h"
+#include <vector>
 
 int main()
 {
-    std::cout << "Red Black Tree" << std::endl;
+    std::cout << "AVL Tree" << std::endl;
+
+    {
+        auto avlTreeTest = [] (std::vector<int>& values)
+            {
+                RedBlackTree<int> tree;
+                std::cout << "Inserting: ";
+                for (int value : values)
+                {
+                    std::cout << value << " ";
+                    tree.Insert(value);
+                }
+                std::cout << std::endl;
+
+                std::cout << "Tree is " << (tree.IsHeightBalanced() ? "height balanced" : 
+                    "not height balanced") << std::endl;
+            };
+
+        avlTreeTest(std::vector<int>{3, 9, 20, 15, 7});
+        avlTreeTest(std::vector<int>{3, 9, 7, 15, 20});
+        avlTreeTest(std::vector<int>{20, 19, 18, 16, 8});
+        avlTreeTest(std::vector<int>{20, 20, 19, 18, 16, 8});
+    }
 
     return 0;
 }
