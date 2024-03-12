4b26345bf14f454384aab7795a7db90e70c0554e
jp <jpgitpublic@gmail.com>
2024-03-12 18:11:18 +1100

    BinaryTree - Removed unnecessary de-referencing of pointer to target node in insertion logic.

--- Trees/BinaryTree/C++/BinaryTree.h
+++ Trees/BinaryTree/C++/BinaryTree.h
@@ -49,7 +49,7 @@ public:
             else
             {
                 newNode = new Node(value);
-                *pointerToTargetNode = &*newNode;
+                *pointerToTargetNode = newNode;
             }
 
             return newNode;
